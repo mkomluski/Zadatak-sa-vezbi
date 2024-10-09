d45300ddb7a53b96e135235bb53b4cd0e0268e24
Komluski <Komluski@DESKTOP-R9864JK>
2024-10-09 23:53:19 +0200

    Add project files.

---
+++ Vezbe1/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Vezbe1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxRedoviPrve = new System.Windows.Forms.TextBox();
+            this.lblKolonePrve = new System.Windows.Forms.Label();
+            this.lblRedoviDruge = new System.Windows.Forms.Label();
+            this.tbxKolonePrve = new System.Windows.Forms.TextBox();
+            this.tbxRedoviDruge = new System.Windows.Forms.TextBox();
+            this.lblKoloneDruge = new System.Windows.Forms.Label();
+            this.tbxKoloneDruge = new System.Windows.Forms.TextBox();
+            this.lblTextZaPrikaz = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxElementiPrve = new System.Windows.Forms.TextBox();
+            this.tbxElementiDruge = new System.Windows.Forms.TextBox();
+            this.btnSubmitElements = new System.Windows.Forms.Button();
+            this.tbxPrikazPrve = new System.Windows.Forms.TextBox();
+            this.tbxPrikazDruge = new System.Windows.Forms.TextBox();
+            this.lblZbir = new System.Windows.Forms.Label();
+            this.tbxZbir = new System.Windows.Forms.TextBox();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unesite broj redova prve matrice:";
+            // 
+            // tbxRedoviPrve
+            // 
+            this.tbxRedoviPrve.Location = new System.Drawing.Point(53, 50);
+            this.tbxRedoviPrve.Name = "tbxRedoviPrve";
+            this.tbxRedoviPrve.Size = new System.Drawing.Size(160, 20);
+            this.tbxRedoviPrve.TabIndex = 1;
+            // 
+            // lblKolonePrve
+            // 
+            this.lblKolonePrve.AutoSize = true;
+            this.lblKolonePrve.Location = new System.Drawing.Point(50, 108);
+            this.lblKolonePrve.Name = "lblKolonePrve";
+            this.lblKolonePrve.Size = new System.Drawing.Size(162, 13);
+            this.lblKolonePrve.TabIndex = 2;
+            this.lblKolonePrve.Text = "Unesite broj kolona prve matrice:";
+            // 
+            // lblRedoviDruge
+            // 
+            this.lblRedoviDruge.AutoSize = true;
+            this.lblRedoviDruge.Location = new System.Drawing.Point(50, 175);
+            this.lblRedoviDruge.Name = "lblRedoviDruge";
+            this.lblRedoviDruge.Size = new System.Drawing.Size(169, 13);
+            this.lblRedoviDruge.TabIndex = 3;
+            this.lblRedoviDruge.Text = "Unesite broj redova druge matrice:";
+            // 
+            // tbxKolonePrve
+            // 
+            this.tbxKolonePrve.Location = new System.Drawing.Point(53, 124);
+            this.tbxKolonePrve.Name = "tbxKolonePrve";
+            this.tbxKolonePrve.Size = new System.Drawing.Size(157, 20);
+            this.tbxKolonePrve.TabIndex = 4;
+            // 
+            // tbxRedoviDruge
+            // 
+            this.tbxRedoviDruge.Location = new System.Drawing.Point(53, 191);
+            this.tbxRedoviDruge.Name = "tbxRedoviDruge";
+            this.tbxRedoviDruge.Size = new System.Drawing.Size(160, 20);
+            this.tbxRedoviDruge.TabIndex = 5;
+            // 
+            // lblKoloneDruge
+            // 
+            this.lblKoloneDruge.AutoSize = true;
+            this.lblKoloneDruge.Location = new System.Drawing.Point(50, 245);
+            this.lblKoloneDruge.Name = "lblKoloneDruge";
+            this.lblKoloneDruge.Size = new System.Drawing.Size(168, 13);
+            this.lblKoloneDruge.TabIndex = 6;
+            this.lblKoloneDruge.Text = "Unesite broj kolona druge matrice:";
+            // 
+            // tbxKoloneDruge
+            // 
+            this.tbxKoloneDruge.Location = new System.Drawing.Point(53, 261);
+            this.tbxKoloneDruge.Name = "tbxKoloneDruge";
+            this.tbxKoloneDruge.Size = new System.Drawing.Size(157, 20);
+            this.tbxKoloneDruge.TabIndex = 7;
+            // 
+            // lblTextZaPrikaz
+            // 
+            this.lblTextZaPrikaz.AutoSize = true;
+            this.lblTextZaPrikaz.Location = new System.Drawing.Point(572, 57);
+            this.lblTextZaPrikaz.Name = "lblTextZaPrikaz";
+            this.lblTextZaPrikaz.Size = new System.Drawing.Size(157, 13);
+            this.lblTextZaPrikaz.TabIndex = 8;
+            this.lblTextZaPrikaz.Text = "Ispod su prikazane obe matrice:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(93, 317);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "POTVRDI";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Kliknite POTVRDI nakon unosa svih podataka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 386);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Unesite elemnte prve matrice odvojene zarezom:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 465);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(248, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Unesite elemente druge matrice odvojene zarezom:";
+            // 
+            // tbxElementiPrve
+            // 
+            this.tbxElementiPrve.Enabled = false;
+            this.tbxElementiPrve.Location = new System.Drawing.Point(53, 402);
+            this.tbxElementiPrve.Name = "tbxElementiPrve";
+            this.tbxElementiPrve.Size = new System.Drawing.Size(233, 20);
+            this.tbxElementiPrve.TabIndex = 13;
+            // 
+            // tbxElementiDruge
+            // 
+            this.tbxElementiDruge.Enabled = false;
+            this.tbxElementiDruge.Location = new System.Drawing.Point(53, 481);
+            this.tbxElementiDruge.Name = "tbxElementiDruge";
+            this.tbxElementiDruge.Size = new System.Drawing.Size(236, 20);
+            this.tbxElementiDruge.TabIndex = 14;
+            // 
+            // btnSubmitElements
+            // 
+            this.btnSubmitElements.Enabled = false;
+            this.btnSubmitElements.Location = new System.Drawing.Point(339, 434);
+            this.btnSubmitElements.Name = "btnSubmitElements";
+            this.btnSubmitElements.Size = new System.Drawing.Size(130, 23);
+            this.btnSubmitElements.TabIndex = 15;
+            this.btnSubmitElements.Text = "POTVRDI ELEMENTE";
+            this.btnSubmitElements.UseVisualStyleBackColor = true;
+            this.btnSubmitElements.Click += new System.EventHandler(this.btnSubmitElements_Click);
+            // 
+            // tbxPrikazPrve
+            // 
+            this.tbxPrikazPrve.Location = new System.Drawing.Point(653, 88);
+            this.tbxPrikazPrve.Multiline = true;
+            this.tbxPrikazPrve.Name = "tbxPrikazPrve";
+            this.tbxPrikazPrve.Size = new System.Drawing.Size(191, 100);
+            this.tbxPrikazPrve.TabIndex = 16;
+            // 
+            // tbxPrikazDruge
+            // 
+            this.tbxPrikazDruge.Location = new System.Drawing.Point(456, 88);
+            this.tbxPrikazDruge.Multiline = true;
+            this.tbxPrikazDruge.Name = "tbxPrikazDruge";
+            this.tbxPrikazDruge.Size = new System.Drawing.Size(191, 100);
+            this.tbxPrikazDruge.TabIndex = 17;
+            // 
+            // lblZbir
+            // 
+            this.lblZbir.AutoSize = true;
+            this.lblZbir.Location = new System.Drawing.Point(519, 317);
+            this.lblZbir.Name = "lblZbir";
+            this.lblZbir.Size = new System.Drawing.Size(128, 13);
+            this.lblZbir.TabIndex = 18;
+            this.lblZbir.Text = "Zbir elemenata matrica je:";
+            // 
+            // tbxZbir
+            // 
+            this.tbxZbir.Location = new System.Drawing.Point(644, 314);
+            this.tbxZbir.Name = "tbxZbir";
+            this.tbxZbir.Size = new System.Drawing.Size(70, 20);
+            this.tbxZbir.TabIndex = 19;
+            // 
+            // btnSum
+            // 
+            this.btnSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.Location = new System.Drawing.Point(510, 213);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(275, 68);
+            this.btnSum.TabIndex = 20;
+            this.btnSum.Text = "SABERI";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 581);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.tbxZbir);
+            this.Controls.Add(this.lblZbir);
+            this.Controls.Add(this.tbxPrikazDruge);
+            this.Controls.Add(this.tbxPrikazPrve);
+            this.Controls.Add(this.btnSubmitElements);
+            this.Controls.Add(this.tbxElementiDruge);
+            this.Controls.Add(this.tbxElementiPrve);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblTextZaPrikaz);
+            this.Controls.Add(this.tbxKoloneDruge);
+            this.Controls.Add(this.lblKoloneDruge);
+            this.Controls.Add(this.tbxRedoviDruge);
+            this.Controls.Add(this.tbxKolonePrve);
+            this.Controls.Add(this.lblRedoviDruge);
+            this.Controls.Add(this.lblKolonePrve);
+            this.Controls.Add(this.tbxRedoviPrve);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxRedoviPrve;
+        private System.Windows.Forms.Label lblKolonePrve;
+        private System.Windows.Forms.Label lblRedoviDruge;
+        private System.Windows.Forms.TextBox tbxKolonePrve;
+        private System.Windows.Forms.TextBox tbxRedoviDruge;
+        private System.Windows.Forms.Label lblKoloneDruge;
+        private System.Windows.Forms.TextBox tbxKoloneDruge;
+        private System.Windows.Forms.Label lblTextZaPrikaz;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxElementiPrve;
+        private System.Windows.Forms.TextBox tbxElementiDruge;
+        private System.Windows.Forms.Button btnSubmitElements;
+        private System.Windows.Forms.TextBox tbxPrikazPrve;
+        private System.Windows.Forms.TextBox tbxPrikazDruge;
+        private System.Windows.Forms.Label lblZbir;
+        private System.Windows.Forms.TextBox tbxZbir;
+        private System.Windows.Forms.Button btnSum;
+    }
+}
+
---
+++ Vezbe1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vezbe1
+{
+    public partial class Form1 : Form
+    {
+
+        private int redoviPrve;
+        private int redoviDruge;
+        private int kolonePrve;
+        private int koloneDruge;
+        private int[,] matrica1;
+        private int[,] matrica2;
+        private int zbir = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (redoviPrve != redoviDruge || kolonePrve != koloneDruge)
+            {
+                throw new ArgumentException("Redovi i kolone se moraju poklapati medjusobno");
+            }
+
+            redoviPrve = int.Parse(tbxRedoviPrve.Text);
+            redoviDruge = int.Parse(tbxRedoviDruge.Text);
+            kolonePrve = int.Parse(tbxKolonePrve.Text);
+            koloneDruge = int.Parse(tbxKoloneDruge.Text);
+
+            matrica1 = new int[redoviPrve, kolonePrve];
+            matrica2 = new int[redoviDruge, koloneDruge];
+
+            tbxElementiPrve.Enabled = true;
+            tbxElementiDruge.Enabled = true;
+            btnSubmitElements.Enabled = true;
+        }
+
+        private void btnSubmitElements_Click(object sender, EventArgs e)
+        {
+            string[] values = tbxElementiPrve.Text.Split(',');
+            string[] values2 = tbxElementiDruge.Text.Split(',');
+
+            if (values.Length != redoviPrve * kolonePrve && values2.Length != redoviDruge * koloneDruge)
+            {
+                throw new ArgumentException("Unesite dovoljno elemnata");
+            }
+
+            int k = 0;
+            for (int i = 0; i < redoviPrve; i++)
+            {
+                for(int j = 0; j < kolonePrve; j++)
+                {
+                    matrica1[i, j] = int.Parse(values[k]);
+                    matrica2[i, j] = int.Parse(values2[k]);
+                    k++;
+                }
+            }
+
+            for (int i = 0; i < redoviPrve; i++)
+            {
+                for (int j = 0; j < kolonePrve; j++)
+                {
+                    tbxPrikazPrve.AppendText(matrica1[i,j].ToString() + ", ");
+                    tbxPrikazDruge.AppendText(matrica2[i, j].ToString() + ", ");
+                }
+                tbxPrikazPrve.AppendText("\r\n");
+                tbxPrikazDruge.AppendText("\r\n");
+            }
+
+            
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < redoviPrve; i++)
+            {
+                for (int j = 0; j < kolonePrve; j++)
+                {
+                    zbir += matrica1[i, j] + matrica2[i, j];
+                }
+            }
+
+            tbxZbir.AppendText(zbir.ToString());
+        }
+    }
+}
